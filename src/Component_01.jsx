0ecc93d91d6ac2673c7543007bf0436b5c70a346
Rajiv Sharma <sharma9769387961@gmail.com>
2025-02-23 11:25:29 +0530

    Remove all the unwanted files and add custom component

---
+++ src/Component_01.jsx
@@ -0,0 +1,7 @@
+function Component_01() {
+    return <div>
+        <center><h1 >Component_01</h1></center>
+    </div>;
+}
+
+export default Component_01;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,6 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
+import Component_01 from "./Component_01.jsx";
+import "./index.css"
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <Component_01/>
 )
